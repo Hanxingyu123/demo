9767e6fb2664163344e23647c6609280e2fc1ce1
hanxingyu <864019463@qq.com>
2020-11-02 19:36:20 +0800

    demo首次提交

---
+++ src/views/Demo.vue
@@ -0,0 +1,34 @@
+<template>
+    <div>
+        <table>
+            <tr>
+                <td>标题1</td>
+                <td>标题2</td>
+            </tr>
+            <tr>
+                <td>内容1</td>
+                <td>内容2</td>
+            </tr>
+        </table>
+    </div>
+</template>
+<script>
+export default {
+    
+}
+</script>
+<style lang="scss" scoped>
+table {
+    width: 500px;
+    border-collapse:collapse;
+    margin: 0 auto;
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    td {
+        border: 1px solid #ccc;
+        height: 50px;
+    }
+}
+</style>
